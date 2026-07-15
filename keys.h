588bb0ed725e1289afe6ad79a6f7e5f7052aa738
TechSanti <ademesonsantiago@gmail.com>
2026-07-15 15:02:50 -0300

    Add files via upload

---
+++ keys.h
@@ -0,0 +1,29 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+#include <stdint.h>
+
+typedef uint8_t byte;
+
+extern const byte answer_08[6];
+extern const byte answer_0D[9];
+extern const byte answer_16[21];
+extern const byte answer_04[6];
+extern const byte answer_02[5];
+extern const byte answer_09[6];
+extern const byte answer_0B[6];
+extern const byte answer_07[6];
+extern const byte answer_03[6];
+extern const byte answer_01[7];
+extern const byte answer_90[11];
+extern const byte psp_81[4];
+
+extern const byte go_key1[16];
+extern const byte go_key2[16];
+extern const byte go_secret[16];
+
+void get_keystore(byte *buffer, byte key);
+void get_challenge1_secret(byte *buffer, byte version);
+void get_challenge2_secret(byte *buffer, byte version);
+
+#endif
